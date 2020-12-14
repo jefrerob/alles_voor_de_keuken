2db74215f729efbf95be1d504e23c17913dacc6c
jefrerob <59914947+jefrerob@users.noreply.github.com>
2020-12-14 17:18:14 +0100

    oef Toevoegen

--- src/main/java/be/vdab/alles_voor_de_keuken/domain/Artikel.java
+++ src/main/java/be/vdab/alles_voor_de_keuken/domain/Artikel.java
@@ -1,9 +1,7 @@
 package be.vdab.alles_voor_de_keuken.domain;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
@@ -11,10 +9,21 @@ import java.math.BigDecimal;
 public class Artikel {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String naam;
     private BigDecimal aankoopPrijs;
     private BigDecimal verkoopPrijs;
+
+    protected Artikel(){
+
+    }
+
+    public Artikel(String naam, BigDecimal aankoopPrijs, BigDecimal verkoopPrijs) {
+        this.naam = naam;
+        this.aankoopPrijs = aankoopPrijs;
+        this.verkoopPrijs = verkoopPrijs;
+    }
 
     public long getId() {
         return id;
--- src/test/java/be/vdab/alles_voor_de_keuken/repositories/JpaArtikelRepositoryTest.java
+++ src/test/java/be/vdab/alles_voor_de_keuken/repositories/JpaArtikelRepositoryTest.java
@@ -1,10 +1,14 @@
 package be.vdab.alles_voor_de_keuken.repositories;
 
+import be.vdab.alles_voor_de_keuken.domain.Artikel;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,10 +18,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Import(JpaArtikelRepository.class)
 class JpaArtikelRepositoryTest extends AbstractTransactionalJUnit4SpringContextTests {
 
-    private  final JpaArtikelRepository repository;
+    private final JpaArtikelRepository repository;
+    private Artikel artikel;
+    private final static String ARTIKELS = "artikels";
 
     public JpaArtikelRepositoryTest(JpaArtikelRepository repository) {
         this.repository = repository;
+    }
+
+    @BeforeEach
+    private void beforeEach(){
+        artikel = new Artikel("test", BigDecimal.ONE,BigDecimal.TEN);
     }
 
     private long idVanTestArtikel1() {
@@ -32,6 +43,13 @@ class JpaArtikelRepositoryTest extends AbstractTransactionalJUnit4SpringContextT
     @Test
     void findByOnbestaandeId() {
         assertThat(repository.findById(-1)).isNotPresent();
+    }
+
+    @Test
+    void create() {
+        repository.create(artikel);
+        assertThat(artikel.getId()).isPositive();
+        assertThat(super.countRowsInTableWhere(ARTIKELS, "id =" + artikel.getId())).isOne();
     }
 
 }
